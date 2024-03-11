686145e73eebe96bb403c44b117d05db96cc877f
Sanjaya Niroula <niroulasanjaya42@gmail.com>
2024-03-12 01:21:20 +0545

    commits to all files | JS added

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Plant-Planet
 A website in which you can get information about the organization and buy the plants you like to help the charity.
+
+
+
+                <div class="product">
+                    <div class="product-image">
+                        <img src="https://images.unsplash.com/photo-1509423350716-97f9360b4e09?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cG90JTIwcGxhbnR8ZW58MHx8MHx8fDA%3D"
+                            alt="" class="p-image">
+                    </div>
+                    <br>
+                    <div class="product-details">
+                        <h4>Smooth Cactus</h4>
+                        <p>Price : <span class="rupees">Rs. 1500</span></p>
+                    </div>
+                </div>
---
+++ script.js
@@ -0,0 +1,41 @@
+var arr = [
+  {
+    name: "Smooth Cactus",
+    imageLink:
+      "https://images.unsplash.com/photo-1509423350716-97f9360b4e09?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cG90JTIwcGxhbnR8ZW58MHx8MHx8fDA%3D",
+    price: "Rs. 1500",
+    availability: "In Stock",
+  },
+  {
+    name: "Tulasi",
+    imageLink:
+      "https://images.unsplash.com/photo-1509423350716-97f9360b4e09?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cG90JTIwcGxhbnR8ZW58MHx8MHx8fDA%3D",
+    price: "Rs. 500",
+    availability: "In Stock",
+  },
+];
+
+function addProducts() {
+  var clutter = "";
+  arr.forEach(function (obj) {
+    clutter += `<div class="product">
+    <div class="product-image">
+        <img src="${obj.imageLink}"
+            alt="" class="p-image">
+    </div>
+    <br>
+    <div class="product-details">
+        <h4>${obj.name}</h4>
+        <p>Price : <span class="rupees">${obj.price}</span></p>
+    </div>
+</div>`;
+
+
+  })
+
+  document.querySelector(".products-gallery").innerHTML= clutter ;
+ 
+}
+
+
+addProducts();
